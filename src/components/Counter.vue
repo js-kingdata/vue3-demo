365c0e2f54b12a26d5c01259124b1dca72a946d2
watsy0007 <watsy0007@gmail.com>
2021-03-29 18:11:05 +0800

    first commit

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,28 +1,32 @@
 <template>
   <div class="counter">
     <button @click="increment(1)">+1</button>
-    {{ count.value }}
+    {{ name }} : {{ value }}
     <button @click="decrement()">-1</button>
   </div>
 
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from "vue";
+import {defineComponent, ref, reactive, toRefs} from "vue";
 import {CounterModel} from "../types/counter";
 
 
 export default defineComponent({
   name: "Counter",
   setup: () => {
-    const count = ref<CounterModel>({value: 111, name: 'counter1'})
+    const useFeatures = () => {
+      const state = reactive<CounterModel>({value: 111, name: 'counter1111'})
+      return toRefs(state)
+    }
+    const {value, name} = useFeatures()
     const increment = (payload: number = 1) => {
-      count.value += payload
+      value.value += payload
     }
     const decrement = (payload: number = 2) => {
-      count.value -= payload
+      value.value -= payload
     }
-    return {count, increment, decrement}
+    return {name, value, increment, decrement}
   }
 })
 
